9a71434c54ca2e593cf863c09ae74b3556fdc334
Charan <153844252+Charan0622@users.noreply.github.com>
2024-06-14 19:07:47 +0530

    Delete Assignments directory

--- Assignments/Week 2/2. Check if a String is a Palindrome.py
+++
@@ -1,7 +0,0 @@
-#Problem 2: Check if a String is a Palindrome
-def is_palindrome(s):
-    return s == s[::-1]
-
-# Taking input from the user
-user_input = input("Enter a string to check if it is a palindrome: ")
-print("Is palindrome:", is_palindrome(user_input))
